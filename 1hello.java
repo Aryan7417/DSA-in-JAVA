3a58edfc50c629388f8dbccca88584938bc569ff
Aryan Yadav <caryan830@gmail.com>
2026-06-01 21:18:02 +0530

    condition Statement

--- 1hello.java
+++ 1hello.java
@@ -29,20 +29,18 @@
 
 //----------------------------------------------------------------------------------
 
-import java.util.*;
+/
+// public class hello1{
+//    public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String name = sc.nextLine();
+//     int a= sc.nextInt();
+//     int b= sc.nextInt();
+//     int sum = a +b;
 
-
-public class hello1{
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
-    int a= sc.nextInt();
-    int b= sc.nextInt();
-    int sum = a +b;
-
-    System.out.println("sum ="+sum + "name ="+name);
-   } 
-}
+//     System.out.println("sum ="+sum + "name ="+name);
+//    } 
+// }
 
 
 
---
+++ conditionStatement2.java
@@ -0,0 +1,101 @@
+// import java.util.Scanner;
+
+// public class conditionStatement2 {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter your name");
+//         int age = sc.nextInt();
+//         if (age>18){
+//             System.out.println("adult");
+//         }
+//         else{
+//             System.out.println("not adult");
+//         }
+
+
+//     }
+// }
+//-------------------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class conditionStatement2{
+
+//     public static void main(String[] args) {
+//         System.out.println("enter number");
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         if(a % 2==0){
+//             System.out.println("even");
+//         }
+//         else{
+//             System.out.println("odd");
+//         }
+
+//     }
+    
+// }
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class conditionStatement2{
+
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("a =" );
+//         int  a = sc.nextInt();
+//         System.out.println("b ");
+//         int b  = sc.nextInt();
+
+//         if(a>b){
+//             System.out.println("a is grater then b");
+//         }
+
+//         else{
+//             if(a<b){
+//                 System.out.println("b is grater then b");
+//             }
+
+//             else{
+//                 System.out.println("a and b are equal");
+//             }
+//         }
+
+
+
+
+//     }
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+import java.util.Scanner;
+
+public class conditionStatement2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  button = sc.nextInt();
+        switch (button) {
+            case 1:System.out.println("sunday");
+                break;
+                case 2 :System.out.println("hello");
+                break;
+                case 3: System.out.println("bonjour");
+                break;
+        
+              
+            default:  System.out.println("Inavalid button");
+                break;
+        }
+    }
+}
+
+
+
+
