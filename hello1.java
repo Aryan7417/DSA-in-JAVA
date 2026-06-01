f70efc6ad00bb30099901b8789209b52ebb65a06
Aryan Yadav <caryan830@gmail.com>
2026-06-01 20:37:31 +0530

    syntex and veriable and datatype

---
+++ hello1.java
@@ -0,0 +1,54 @@
+
+//SYNTEX 
+
+// class hello1{
+//     public static void main(String[] args) {
+//         System.out.println("HEllo World");
+//     }
+// }
+
+//---------------------------------------------------------------
+// public class hello1 {
+//     public static void main(String[] args) {
+//         System.out.println("#\n##\n###\n####");
+//     }
+// }
+//-----------------------------------------------
+
+
+// class hello1{
+//     public static void main(String[] args) {
+
+//         int a =10;
+//         int b =5;
+
+
+//         System.out.println((a*b)/(a-b));
+//     }
+// }
+
+//----------------------------------------------------------------------------------
+
+import java.util.*;
+
+
+public class hello1{
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    int a= sc.nextInt();
+    int b= sc.nextInt();
+    int sum = a +b;
+
+    System.out.println("sum ="+sum + "name ="+name);
+   } 
+}
+
+
+
+
+
+
+
+
+
