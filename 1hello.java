c4514e53eea35ceb8fbea443c5454ea2373e60d3
Aryan Yadav <caryan830@gmail.com>
2026-06-01 23:53:30 +0530

    loops

--- 1hello.java
+++ 1hello.java
@@ -29,7 +29,7 @@
 
 //----------------------------------------------------------------------------------
 
-/
+
 // public class hello1{
 //    public static void main(String[] args) {
 //     Scanner sc = new Scanner(System.in);
