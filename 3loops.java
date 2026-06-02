b629baecfd6eb4a6dd8f9771f5fc7f530cd9fa32
Aryan Yadav <caryan830@gmail.com>
2026-06-02 17:05:04 +0530

    pattern question

---
+++ 3loops.java
@@ -0,0 +1,63 @@
+// public class loops3 {
+//     public static void main(String[] args) {
+        
+//         String a = "Aryan";
+        
+//         for(int i =1;i<11;i++){
+//             System.out.println(a+i);
+//         }
+        
+//     }
+// }
+//--------------------------------------------------------------------------------------------------------------------
+
+// public class loops3{
+//     public static void main(String[] args) {
+//         int i = 0;
+//         while (i<10) {
+//             System.out.println("paridhi");
+            
+//         }
+//     }
+// }
+
+//-----------------------------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class loops3 {
+
+//     public static void main(String[] args) {
+//         Scanner sc =  new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int sum = 0;
+//         for (int i=0; i<=n; i++){
+//             sum = sum + i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+//------------------------------------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class loops3 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         for(int i =1;i<=10;i++){
+//             System.out.println(i*a);
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
---
+++ patterns4.java
@@ -0,0 +1,188 @@
+// public class patterns4 {
+//      public static void main(String[] args) {
+//         for(int i =1;i<=4;i++){
+//             for(int j = 1;j<=5;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//      }
+// }
+
+//-----------------------------------------------------------------------------------
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+        
+//         int a=6;
+//         int b =5;
+
+//         for(int i=1;i<=a;i++){
+//             for(int j =1;j<=b;j++){
+//                 if(i==1 || j==1|| i==a||j==b){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+
+//             }
+//             System.out.println("");
+//         }
+         
+
+
+//     }
+// }
+//------------------------------------------------------------------------------------------
+
+
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+
+//         for(int i =1;i<=4;i++){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+//------------------------------------------------------------------------------
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+        
+//         for(int i =4;i>=1;i--){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//---------------------------------------------------------------------------
+
+
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+//         int n =7;
+
+//         for(int i=1;i<=n;i++){
+//             for(int j =1;j<=n-i;j++ ){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1;j<=i;j++){
+//                 System.out.println("*");
+//             }
+//         }
+//         System.out.println("");
+//     }
+// }
+
+
+//------------------------------------------------------------------------------
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+//     for(int i =1;i<=6;i++){
+//         for(int j = 1;j<=i;j++){
+//             System.out.print(j+" ");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+
+
+//------------------------------------------------------------------------------
+
+
+
+
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+//         for(int i = 1 ;i<=6;i++){
+//             for(int j = 1;j<6-i+1;j++){
+
+//                 System.out.print(j);
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+//---------------------------------------------------------------------------------------
+
+
+
+
+// public class patterns4 {
+
+//     public static void main(String[] args) {
+//         int  number=1;
+        
+//         for(int i =1;i<=5;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(" " +number);
+//                 number++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+public class patterns4 {
+
+    public static void main(String[] args) {
+        for(int i = 1; i<=5;i++){
+            for(int j= 1 ; j<=i;j++){
+                int sum = i+j;
+                if(sum % 2 ==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
