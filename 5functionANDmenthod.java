4f6ca835ae3e23902e9dd1f8a9c391e24e704035
Aryan Yadav <caryan830@gmail.com>
2026-06-04 13:37:59 +0530

    function and method

---
+++ 5functionANDmenthod.java
@@ -0,0 +1,99 @@
+// import java.util.Scanner;
+
+// public class functionANDmenthod {
+
+//     public static  void printName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//         public static void main(String[] args) {
+//             Scanner sc = new Scanner(System.in);
+//             String name = sc.next();
+            
+//             System.out.println(name);       
+//         }
+// }
+
+//-------------------------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class functionANDmenthod {
+
+//     public static int addtion(int a,int b){
+//         int sum =a + b;
+//         return sum;
+//     }
+
+//      public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter first number :");
+//         int a  = sc.nextInt();
+//         System.out.println("eneter second number:");
+//         int b = sc.nextInt();
+
+//         int sum = addtion(a,b);
+//         System.out.println("sum of two number="+sum);
+        
+//      }
+// }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class functionANDmenthod {
+
+//     public static int multiplication(int a , int b){
+//         return a *b;
+//     }    
+//     public static void main(String[] args) {
+       
+//         Scanner sc = new Scanner(System.in);
+//         int mult1= sc.nextInt();
+//         int mult2=sc.nextInt();
+        
+//         int mult = multiplication(mult1, mult2);
+//             System.out.println("multipliaction of two numbver :"+mult);
+        
+//     }
+// }
+
+import java.util.Scanner;
+
+public class functionANDmenthod {
+
+
+    public static void fcatorial(int a){
+        int fact = 1;
+
+        if(a<0){
+            System.out.println("invalid number");
+            return;
+        }
+        for(int i = a;i>=1;i--){
+
+            fact = fact*i;
+
+        }
+        System.out.println(fact);
+        return;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+         fcatorial(a);
+        // System.out.println(fact);
+
+
+
+        
+    }
+}
+
+
+
