fa6bbfc6e0a8f206eb418879e6ce5cf048f88dfc
Aryan Yadav <caryan830@gmail.com>
2026-06-10 21:40:56 +0530

    2D array

--- 7Array.java
+++ 7Array.java
@@ -44,38 +44,45 @@
 
 //---------------------------------------------------------------------------------------------------
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
- 
+//---------------LINEAR SEARCH-------------------------------------------- 
 
-public class Array7 {
+// public class Array7 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int arr[] =new int[a];
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int arr[] =new int[a];
 
-        for(int i = 0 ;i<a;i++){
-            arr[i]=sc.nextInt();
-        }
+//         for(int i = 0 ;i<a;i++){
+//             arr[i]=sc.nextInt();
+//         }
 
-        int x = sc.nextInt();
+//         int x = sc.nextInt();
 
-        for(int i =0 ;i<arr.length;i++){
-            if(arr[i] == x){
-                System.out.println("X is found a index :"+ i);
-            }
-        }
+//         for(int i =0 ;i<arr.length;i++){
+//             if(arr[i] == x){
+//                 System.out.println("X is found a index :"+ i);
+//             }
+//         }
+//     }
+// }
 
-
-
-
-
-
-    }
-}
+//-----------------------------------------------------------------------------------
 
 
+
+
+
+
+
+
+
+
+
+
+  
 
 
 
---
+++ 8twoDArray.java
@@ -0,0 +1,97 @@
+// import java.util.Scanner;
+
+// public class twoDArray {
+//     public static void main(String[] args) { 
+//         Scanner sc = new Scanner(System.in);
+//         int row = sc.nextInt();
+//         int cols = sc.nextInt();
+
+//         int [][] number = new int [row][cols];
+
+//         for(int i =0 ; i < row ; i++){
+//             for(int j = 0; j < cols ; j++){
+//                 number[i][j]=sc.nextInt();
+
+//             }
+
+//         }
+//         for(int i =0 ;i<row;i++){
+//             for(int j = 0;j<cols;j++){
+//                 System.out.print(number[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+         
+//     }
+    
+// }
+
+
+
+//------------------------------------------------------------------------------------
+
+import java.util.Scanner;
+
+public class twoDArray {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int cols = sc.nextInt();
+
+
+        int number[][] = new int[row][cols];
+
+
+        for(int i = 0 ; i<row ; i++){
+            for(int j = 0 ; j<cols;j++){
+                number[i][j] = sc.nextInt();
+                
+            }
+        }
+
+        int x= sc.nextInt();
+        for(int i = 0; i<row;i++) {
+            for(int j = 0 ; j<cols;j++){
+                if(number[i][j] == x){
+                    System.out.println("X is found at loaction ("+i + ","+j+ ")");
+                }
+            }
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
