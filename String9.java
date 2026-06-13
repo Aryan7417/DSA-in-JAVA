b7217e6de9ba9f91226eebb3985f4d54dd79cbdd
Aryan Yadav <caryan830@gmail.com>
2026-06-13 13:28:53 +0530

    concept of String

--- String9.java
+++ String9.java
@@ -1,3 +1,59 @@
+import java.util.Scanner;
+
 public class String9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //-----------------------------------------------------------------
+        // System.out.print("Enter your name ");
+        // String name = "Aryan  YADAV";
+        // String a = sc.nextLine();
+        // System.out.println("Your name is :"+a);
+        // System.out.println("second palen b :"+name);
+        //--------------------------------------------------------------
+
+
+        //---------------------------concatenation---------------------
+
+        // System.out.print("Enter your first name is : ");
+        // String f_name = sc.next();
+        // System.out.print("Enter your last name : ");
+        // String L_name = sc.next();
+        // String full_name  = f_name +" " +L_name;
+        // System.out.println("you name is :  " + full_name);
+
+        // System.out.println(full_name.length());
+
+        // //charAt
+        // for(int i = 0;i<full_name.length();i++){
+        //     System.out.println(full_name.charAt(i));
+        // }
+
+        //-----------------------------------------------------
+        // String name1="tony";
+        // String name2= "tony ";
+
+        // if(name1.compareTo(name2)==0){
+        //     System.out.println("String are erqual");
+        // }
+        // else{
+        //     System.out.println("STring is not eaula");
+        // }
+
+        //-------------------------------------------------------------------
+        String sen= "ARyan";
+        String name= sen.substring(4);
+        System.out.println(name);
+        
+
+
+
+
+
+
+
+
+
+
+    }
     
 }
