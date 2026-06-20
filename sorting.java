4637e2770f5f52319cb1c24c3cec072476e4ce97
Aryan Yadav <caryan830@gmail.com>
2026-06-21 01:44:59 +0530

    introduction of sorting

---
+++ sorting.java
@@ -0,0 +1,66 @@
+// public class sorting {
+//     public static void printArr(int arr[]) {
+//         for(int i =0;i<arr.length;i++){
+//             System.out.println(arr[i]+" ");
+//         }
+        
+//     }
+
+
+//    public static void main(String[] args) {
+
+  //      int arr[]={74,85,24,52,56,8,3,8,45,789,23,78,21323};
+        
+        //----------------bubble short----------------------
+
+        // for(int i = 0;i<arr.length-1;i++){
+        //     for(int j=0;j<arr.length-i-1;j++){
+        //         if(arr[j]>arr[j+1]){
+        //             //sawp
+        //             int temp=arr[j];
+        //             arr[j]=arr[j+1];
+        //             arr[j+1]=temp;
+        //         }
+        //     }
+        // }
+        // printArr(arr);
+
+        //------------------------selection sort---------------
+
+        // for(int i =0;i<arr.length-1;i++){
+        //     int smallest=i;
+        //     for(int j = i+1;j<arr.length;j++){
+        //         if(arr[smallest]>arr[j]){
+        //             smallest =j;
+        //         }
+        //     }
+        //     int temp = arr[smallest];
+        //     arr[smallest]=arr[i];
+        //     arr[i]=temp;
+        // }
+        // printArr(arr);
+     
+        //-------------------------inseration sort---------------------
+
+
+
+        // for(int i =1;i<arr.length;i++){
+        //     int current = arr[i];
+        //     int j = i-1;
+        //     while(j >=0 && current<arr[j]){
+        //         arr[j+1] =arr[j];
+        //         j--;
+        //     }
+        //     //placement
+        //     arr[j+1]=current;
+        // }
+        // printArr(arr);
+    
+        
+    
+    
+    
+    
+    //}
+    
+//}
